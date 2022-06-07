58e5fbc38bf348004928697c6d0dea4a076b016d
Edu <particularspam@gmail.com>
2022-06-07 02:16:32 -0500

    Added Figuras formulas

--- figuras.js
+++ figuras.js
@@ -1,8 +1,59 @@
+
+// Código del cuadrado
+console.group("Cuadrados");
 const ladoCuadrado = 5;
-console.log(`El lado del cuadrado es ${ladoCuadrado}`);
+console.log(`El lado del cuadrado es ${ladoCuadrado}` + ` cm`);
 
 const perimetroCuadrado = ladoCuadrado * 4;
-console.log(`El perimetro del cuadrado es ${perimetroCuadrado}`);
+console.log(`El perímetro del cuadrado es ${perimetroCuadrado}` + ` cm`);
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log(`El area del cuadrado es ${areaCuadrado}`);
+console.log(`El área del cuadrado es ${areaCuadrado}` + ` cm^2`);
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const BaseTriangulo = 4;
+const alturaTriangulo = 5.5;
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + BaseTriangulo;
+const areaTriangulo = (BaseTriangulo * alturaTriangulo) / 2;
+
+console.log("Los lados del triángulo miden: "
++ ladoTriangulo1
++ " cm, "
++ ladoTriangulo2
++ " cm y "
++ BaseTriangulo
++ " cm"
+);
+console.log("La altura del triángulo es: " + alturaTriangulo + " cm");
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
+console.log("El área del triángulo es: " + areaTriangulo + " cm^2");
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio del círculo
+const radioCirculo = 4;
+console.log("El radio del círculo es: " + radioCirculo + " cm");
+
+// Diametro del círculo
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del círculo es: " + diametroCirculo + " cm");
+
+// Pi = 3.1416
+const PI = Math.PI;
+console.log("El valor de PI es: " + PI);
+
+// Circunferencia del círculo
+const circunferenciaCirculo = 2 * PI * radioCirculo;
+console.log("La circunferencia del círculo es: " + circunferenciaCirculo + " cm");
+
+// Área del círculo
+const areaCirculo = PI * radioCirculo * radioCirculo;
+console.log("El área del círculo es: " + areaCirculo + " cm^2");
+
+console.groupEnd();
