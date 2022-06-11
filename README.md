3a2adc48f398b7acf8349cbfcaf4851413e47fd6
Edu <particularspam@gmail.com>
2022-06-11 16:43:01 -0500

    Añadido Descuentos

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 2) Implementar fórmulas en JS
 3) Crear funciones
 4) Integrar JS con HTML
+
+
+## Taller #2: Porcentajes y descuentos
+
+1) Definir las fórmulas
+2) Implementar fórmulas en JS
+3) Crear funciones
+4) Integrar JS con HTML
--- isosceles.js
+++ isosceles.js
@@ -1,18 +1,20 @@
 function calculo(params) {
-    const a = parseInt(document.getElementById("input1").value);
-    const b = parseInt(document.getElementById("input2").value);
-    const c = parseInt(document.getElementById("input3").value);
+    const a = parseInt(document.getElementById("input1").value); // Lado A
+    const b = parseInt(document.getElementById("input2").value); // Lado B
+    const c = parseInt(document.getElementById("input3").value); // Base
  
-    if (a == 0 || b == 0 || c == 0) {
-        alert("No se puede calcular");
+    if (a <= 0 || b <= 0 || c <= 0) {
+        alert("No se puede calcular porque no son números positivos");
     }
     else {
         if (a == b && b == c) {
             alert("Triángulo equilátero");
         }
         else if (a == b || b == c || a == c) {
-            const altura = Math.sqrt(a**2 + b**2);
-            alert("El triángulo es isósceles y su altura es: " + altura);
+            if (a === b && a != c) {
+                const altura = Math.sqrt(a**2 - c**2);
+                alert("El triángulo es isósceles y su altura es: " + altura);
+            }
         }
         else {
             alert("El triángulo es escaleno");
