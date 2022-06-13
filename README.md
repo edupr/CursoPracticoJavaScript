e86993d3e395047bb61517c4ae3a622eff04d457
Edu <particularspam@gmail.com>
2022-06-12 19:50:25 -0500

    Cálculo del Promedio

--- README.md
+++ README.md
@@ -16,3 +16,11 @@
 2) Implementar fórmulas en JS
 3) Crear funciones
 4) Integrar JS con HTML
+
+
+## Taller #2: Promedio, mediana y moda
+
+1) Definir las fórmulas
+2) Implementar fórmulas en JS
+3) Crear funciones
+4) Integrar JS con HTML
---
+++ promedio.js
@@ -0,0 +1,20 @@
+
+
+const promedioLista1 = sumaLista1 / lista1.length;
+console.log(promedioLista1);
+
+function calcularMediaAritmetica(lista) {
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        });
+
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
