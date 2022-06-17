0a29d84603a594fc59e1e7f663be0349c54f59b1
Edu <particularspam@gmail.com>
2022-06-16 19:18:06 -0500

    Añadida función Mediana salarial

---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Lalo",
+    salary: 1500,
+})
+colombia.push({
+    name: "Fer",
+    salary: 500,
+})
+colombia.push({
+    name: "María",
+    salary: 800,
+})
+colombia.push({
+    name: "Donald",
+    salary: 900,
+})
+colombia.push({
+    name: "Juan",
+    salary: 1100,
+})
+colombia.push({
+    name: "Carlos",
+    salary: 750,
+})
+colombia.push({
+    name: "Oscar",
+    salary: 500,
+})
+colombia.push({
+    name: "Pedro",
+    salary: 3200,
+})
+colombia.push({
+    name: "Natalia",
+    salary: 6500,
+})
+colombia.push({
+    name: "Diego",
+    salary: 2300,
+})
+colombia.push({
+    name: "Pablo",
+    salary: 400,
+})
+colombia.push({
+    name: "Erika",
+    salary: 600,
+})
+colombia.push({
+    name: "Majo",
+    salary: 1150,
+})
+colombia.push({
+    name: "Laura",
+    salary: 4200,
+})
+colombia.push({
+    name: "Giovanna",
+    salary: 2700,
+})
+colombia.push({
+    name: "Gema",
+    salary: 1500,
+})
+colombia.push({
+    name: "Nuria",
+    salary: 800,
+})
+colombia.push({
+    name: "Rosa",
+    salary: 1100,
+})
+colombia.push({
+    name: "Héctor",
+    salary: 400,
+})
+colombia.push({
+    name: "Diego",
+    salary: 1500,
+})
